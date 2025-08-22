7ae83c467ee59db286d1d7a8cfe83919f4133982
Rafael Lopez <rafael.ralh@gmail.com>
2025-08-22 12:07:49 -0600

    Platform base, including card component and in Linux

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import './General.css'
+import './App.css'
+import './UI/Card.css'
+
+import ReactIcon from "./assets/react.svg"
+
+import Card from './UI/Card'
+
+function App() {
+
+  return (
+    <>
+      <div id="topbar">
+        <div id="leftSide">
+              <button disabled>Home</button> 
+        </div>
+        <div id="centerSide">
+            <h1 id="title">Personal projects</h1>
+        </div>
+        <div id="rightSide">
+            <button>Button</button>
+            <button>Button</button>
+        </div>
+    </div>
+    <div id="content">
+        <ul>
+            <h2>Udemy</h2>
+            <h3>Angela Yu</h3>
+                <Card snapshot={ReactIcon} projectName='WW' projectDescription='ss'/>
+                <Card projectName='Coming soon'/>
+        </ul>
+        
+    </div>
+    <div id="footer">
+        <div id="leftSide">
+            <p>Rafael Agustín López Hernández, 2025</p> 
+        </div>
+        <div id="rightSide">
+            <button>LinkedIn</button>
+            <button>Email</button>
+            <button>Curriculum Vitae</button>
+            <button>Button</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/UI/Card.tsx
@@ -0,0 +1,29 @@
+interface CardProps{
+    snapshot?: string|null,
+    projectName: string,
+    projectDescription?: string|null
+}
+
+export default function Card(props: CardProps) {
+    
+    return(
+        <li>
+            <div id="card">
+                <div id="card_leftSide">
+                    <div></div>
+                    {props.snapshot !== null ? <img src={props.snapshot}></img> : <></>}
+                    
+                </div>
+                <div id="card_rightSide">
+                    <div id='cardTitle'>
+                        {props.projectName}
+                    </div>
+                    <div id="cardDescription">
+                        {props.projectDescription !== null ? props.projectDescription : "No description"}
+                    </div>
+                    {/* <a className="diceeLink" href="./Dicee/dicee.html">Dicee</a> */}
+                </div>
+            </div>
+        </li>
+    )
+}
