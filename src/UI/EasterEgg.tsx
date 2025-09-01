b37b30556726394315a6b6b9231445c192108d0b
Rafael Agustin López Hernández <donrafiki@Rafaels-Office-Mac-mini.local>
2025-09-01 07:39:03 -0600

    Added more EasterEgg stuff

--- src/UI/EasterEgg.tsx
+++ src/UI/EasterEgg.tsx
@@ -8,13 +8,21 @@ export default function EasterEgg(props: EasterEggProps) {
 
     const [clickCounter, setClickCounter] = useState<number>(0);
 
-    function counter() : number{
-        return 0;
+    const counter = () => {
+        setClickCounter(clickCounter + 1);
+        console.log("Counter: " + clickCounter);
     }
 
     return(
         <>
-            <img src={props.snapshot} ></img>
+            <img src={props.snapshot} onClick={counter}></img>
+            {clickCounter === 11 ? (
+                <div>
+                    <h1>This is an easter egg, shhhh</h1>
+                    <div>
+                        <img></img>
+                    </div>
+                </div>): <></>}
         </>
     )
 }
